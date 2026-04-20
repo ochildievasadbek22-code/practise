1be2212edee8010b8990fca34c65a7f42ff65660
Rudy <ochildievasadbek22@gmail.com>
2026-04-20 12:57:32 +0900

    fix: Python magic methods

--- class.about.py
+++ class.about.py
@@ -50,3 +50,48 @@ print("new_message", new_message)
 
 # static method
 Person.explain()
+
+
+print("======= special/magic methods ========")
+# Python's most common special methods are below:
+# __init__ __new__ __str__ __call__ __getitem__ __eq__ __len__    ....
+
+
+class Car():
+    # state
+    description = "This class makes cars"
+
+    # constructor
+    def __new__(cls, *args):
+        print("*__new__*")
+        return super().__new__(cls)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    # method
+    def start_engine(self):
+        print(f"the {self.name} started engine!")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped engine!")
+
+    def __str__(self):
+        return f"the car name : {self.name} was produced {self.year} year"
+
+    def __call__(self):
+        print("Object is called as function")
+        return True
+
+
+my_car = Car("Ferari", 2025)
+my_car.start_engine()
+my_car.stop_engine()
+
+print("--------")
+your_car = Car("Toyota", 2026)
+print(your_car)
+your_car()  # look like function
+response = your_car()
+print("response:", response)
