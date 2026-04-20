50769c568844f46bb0c7cff0fa390de988a00f87
Rudy <ochildievasadbek22@gmail.com>
2026-04-20 11:01:30 +0900

    feat: study iterable objects

--- train.js
+++ train.js
@@ -1,3 +1,19 @@
+/* A-TASK: 
+Savol:Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+*/
+
+
+/* Masalaning yechimi: */
+
+
+
+
+
+
+   
+
+
 
 /* A-TASK: 
 Savol: Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashgan sonini return qilishi kerak boladi.
@@ -5,15 +21,15 @@ MASALAN countLetter("e", "engineer") 3ni return qiladi.
 */
 
 /* Masalaning yechimi: */
-function task(harf, suz) {
-  let countValue = 0;
-  for (let a = 0; a < suz.length; a++) {
-    if (suz[a] == harf) {
-      countValue++
-    }
-  }
-  return countValue;
-}
-result = task("s", "successfull")
-console.log(result)
+// function task(harf, suz) {
+//   let countValue = 0;
+//   for (let a = 0; a < suz.length; a++) {
+//     if (suz[a] == harf) {
+//       countValue++
+//     }
+//   }
+//   return countValue;
+// }
+// result = task("s", "successfull")
+// console.log(result)
 
