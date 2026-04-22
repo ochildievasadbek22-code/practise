54f681d2902f5f6dc8b8e591e98cbdbcaeb6a392
Rudy <ochildievasadbek22@gmail.com>
2026-04-22 09:05:54 +0900

    feat: Task C

--- train.js
+++ train.js
@@ -1,19 +1,50 @@
-/* B-TASK: 
+/* C-TASK: 
+Savol: Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+*/
+
+// Masalaning yechimi: 
+
+function checkContent(a, b) {
+  const sort1 = a.split("")
+  const sort2 = b.split("")
+  const q = sort1.sort()
+  const p = sort2.sort()
+  const x = p.join('')
+  const y = q.join('')
+  return x === y;
+}
+const result = checkContent('mitgroup', 'gmtiprou')
+console.log(result)
+
+
+
+
+
+
+
+
+
+//========================================================================================
+
+
+
+/* B-TASK:
 Savol:Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 */
 
 // Masalaning yechimi: 
-function countDigits(str) {
-  con = 0;
-  for (let h of str) {
-    if (h >= "0" && h <= "9")
-      con++
-  }
-  return con
-}
-const result = countDigits("ad2a54y79wet0sfgb9")
-console.log(result)
+// function countDigits(str) {
+//   con = 0;
+//   for (let h of str) {
+//     if (h >= "0" && h <= "9")
+//       con++
+//   }
+//   return con
+// }
+// const result = countDigits("ad2a54y79wet0sfgb9")
+// console.log(result)
 
 //========================================================================================
 
