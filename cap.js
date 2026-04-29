91db2816d7cf60e8fdba4d23d3d55c54a2d18d88
Rudy <ochildievasadbek22@gmail.com>
2026-04-29 09:56:18 +0900

    advantages of callback and promise

---
+++ cap.js
@@ -0,0 +1,30 @@
+/* asynchronous functions: callback vs Async vs Promise
+
+       DEFINE              CALL 
+       callback            callback
+       asynch/await         them/catch
+       promise              them/catch
+*/
+
+
+
+// define -asynchronous function
+function division(a, b) {
+    return new Promise((resolve, reject) => {
+        if (b === 0) {
+            reject("Not divided by zero", null);
+    } else {
+        setInterval(function(){
+            resolve (a % b);
+        }, 2000);
+    }
+    })
+    }
+   
+// call -asynchronous function
+division(10, 3,).then(data => {
+     console.log("Result:", data)
+     console.log(".......")
+}).catch(err => {
+    console.log("Error division:", err)
+}) 
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+console.log("Web server start")
 const express = require('express')
 const app = express()
 
 // Traditional Get API
 app.get('/', (req, res) => {
+  console.log("/ get")
     res.send('<h1>Hello World!</h1>')
 })
 
 // Rest API
 app.get('/rest', (req, res) => {
+    console.log("/rest get")
   res.json({name: "Marco", age: 21, group: "MIT"})
 })
 
