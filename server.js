6ce64744a7590c6de96bec47c581bba25fd226d0
Rudy <ochildievasadbek22@gmail.com>
2026-04-29 09:08:31 +0900

    feat: building simple backend server on nodejs

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+// Traditional Get API
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+// Rest API
+app.get('/rest', (req, res) => {
+  res.json({name: "Marco", age: 21, group: "MIT"})
+})
+
+app.listen(3000, () => {
+  console.log(`Backend server is running on port 3000`)
+})
