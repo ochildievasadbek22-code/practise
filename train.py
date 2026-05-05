c4228adb77f136a4107c62bd85f54190e1ed52a0
Rudy <ochildievasadbek22@gmail.com>
2026-05-06 04:47:17 +0900

    feat: task

--- train.py
+++ train.py
@@ -1,3 +1,10 @@
+
+# I-TASK (Python)
+
+# Savol: Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+# MASALAN: get_digits("m14i1t") return qiladi "141"
+# Masalaning yechimi:
+
 def get_digits(a):
     return ''.join(filter(str.isdigit, a))
 
