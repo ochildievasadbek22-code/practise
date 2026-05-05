7a44533c2d58dc417fb22d53608ebdec41dc5e2e
Rudy <ochildievasadbek22@gmail.com>
2026-05-06 04:58:43 +0900

    feat: task

--- train.py
+++ train.py
@@ -11,3 +11,21 @@ def get_digits(a):
 
 result = get_digits("m14i1t")
 print(result)
+
+
+# ======================================================================================================================
+
+
+# G-TASK (Python)
+
+# Savol: Shunday function tuzingki unga integerlardan iborat array pass bolsin
+# va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+# MASALAN: get_highest_index([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+
+# Masalaning yechimi:
+
+# def get_highest_index(array):
+#     return array.index(max(array))
+
+# result = get_highest_index([5, 21, 12, 21, 8])
+# print(result)
