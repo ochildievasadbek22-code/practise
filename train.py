974720d4434833662f1e91c56d3d705f8fe80982
Rudy <ochildievasadbek22@gmail.com>
2026-05-14 09:42:47 +0900

    feat: MIT Task M

--- train.py
+++ train.py
@@ -1,16 +1,36 @@
 
+
+# M-TASK (PYTHON)
+
+# Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+# MASALAN: palindrom_check("dad") return True;  palindrom_check("son") return False;
+
+
+# Masalaning yechimi:
+
+def palindrom_check(a):
+    return a == a[::-1]
+
+
+print(palindrom_check("dad"))
+print(palindrom_check("son"))
+
+
+# ======================================================================================================================
+
+
 # I-TASK (Python)
 
 # Savol: Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
 # MASALAN: get_digits("m14i1t") return qiladi "141"
 # Masalaning yechimi:
 
-def get_digits(a):
-    return ''.join(filter(str.isdigit, a))
+# def get_digits(a):
+#     return ''.join(filter(str.isdigit, a))
 
 
-result = get_digits("m14i1t")
-print(result)
+# result = get_digits("m14i1t")
+# print(result)
 
 
 # ======================================================================================================================
